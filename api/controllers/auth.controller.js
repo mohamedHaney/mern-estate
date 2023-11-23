bad22062667b76adbecb59c20b48c5ac87a22eff
mohamedHaney <mohaney745@gmail.com>
2023-11-24 01:42:45 +0200

    create middle ware and function to handel the err

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,9 @@
 import User from "../models/User.model.js"
 import bcrybt from "bcrypt"
+import { errorHandler } from "../utils/error.js"
 
 //user controller
-export const signup = async(req,res)=>{
+export const signup = async(req,res,next)=>{
   const {username , email , password} = req.body
   const hashedPassword = bcrybt.hashSync(password,10)
   const newUser = new User({username,email,password:hashedPassword})
@@ -10,7 +11,7 @@ export const signup = async(req,res)=>{
     await newUser.save()
     res.status(201).json({message:'user created successfully'})
   }catch(error){
-    res.status(500).json(error.message)
+    next(error)
   }
 
 }
