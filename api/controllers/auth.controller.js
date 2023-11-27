80749ea6820128afa58349187092ffbc6ed41fc2
mohamedHaney <mohaney745@gmail.com>
2023-11-27 14:02:17 +0200

    #

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,7 +3,7 @@ import bcrybt from "bcrypt"
 import { errorHandler } from "../utils/error.js"
 import jwt from 'jsonwebtoken'
 
-//user controller
+//Sign Up controller
 export const signup = async(req,res,next)=>{
   const {username , email , password} = req.body
   const hashedPassword = bcrybt.hashSync(password,10)
@@ -19,12 +19,12 @@ export const signin = async(req,res,next)=>{
   const { email , password} = req.body
   try{
     const validUser = await User.findOne({email})
-    if(!validUser)return next(errorHandler(404,'user not found'))
+    if(!validUser) return next(errorHandler(404,'user not found'))
     const validPassword = bcrybt.compareSync(password , validUser.password)
   if(!validPassword) return next(errorHandler(401,'wrong credentials!'))
   const token = jwt.sign({id:validUser._id},process.env.JWT_SECTET)
-  const{password:pass,...rest}=validUser._doc
-  res.cookie('access_token',token,{httpOnly:ture}).status(200).json(rest)
+  const{password:pass, ...rest}=validUser._doc
+  res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest)
 }catch(error){
     next(error)
   }
