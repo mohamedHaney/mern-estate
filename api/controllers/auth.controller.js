38eb4ca97d10bf3280e9c7f52bb99aace2066a67
mohamedHaney <mohaney745@gmail.com>
2023-11-27 03:18:30 +0200

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/User.model.js"
 import bcrybt from "bcrypt"
 import { errorHandler } from "../utils/error.js"
+import jwt from 'jsonwebtoken'
 
 //user controller
 export const signup = async(req,res,next)=>{
@@ -13,5 +14,18 @@ export const signup = async(req,res,next)=>{
   }catch(error){
     next(error)
   }
-
+}
+export const signin = async(req,res,next)=>{
+  const { email , password} = req.body
+  try{
+    const validUser = await User.findOne({email})
+    if(!validUser)return next(errorHandler(404,'user not found'))
+    const validPassword = bcrybt.compareSync(password , validUser.password)
+  if(!validPassword) return next(errorHandler(401,'wrong credentials!'))
+  const token = jwt.sign({id:validUser._id},process.env.JWT_SECTET)
+  const{password:pass,...rest}=validUser._doc
+  res.cookie('access_token',token,{httpOnly:ture}).status(200).json(rest)
+}catch(error){
+    next(error)
+  }
 }
