6565433b434076bfd1abe1d286657946e2b23158
mohamedHaney <mohaney745@gmail.com>
2023-12-29 04:46:57 +0200

    create get user listings api routes

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,14 +1,15 @@
-import User from "../models/User.model.js"
-import { errorHandler } from "../utils/error.js"
-import bcrypt from 'bcrypt'
-export const test =(req,res)=>{
+import User from "../models/User.model.js";
+import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcrypt from "bcrypt";
+export const test = (req, res) => {
   res.json({
-    message:'api route is working'
-  })
-}
+    message: "api route is working",
+  });
+};
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
-    return next(errorHandler(401, 'You can only update your own account!'));
+    return next(errorHandler(401, "You can only update your own account!"));
   try {
     if (req.body.password) {
       req.body.password = bcrypt.hashSync(req.body.password, 10);
@@ -32,13 +33,29 @@ export const updateUser = async (req, res, next) => {
   }
 };
 
-export const deleteUser = async(req,res,next)=>{
-  if (req.user.id !== req.params.id) return next(errorHandler(401,'You Can Only Delete Your Own Account'))
-    try{
-      await User.findByIdAndDelete(req.params.id)
-      res.f('access_token')
-      res.status(200).json('user has been deleted!')
-    }catch(error){
-      next(error)
-    }
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You Can Only Delete Your Own Account"));
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.f("access_token");
+    res.status(200).json("user has been deleted!");
+  } catch (error) {
+    next(error);
   }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if(req.user.id === req.params.id){
+  try{
+    const listings = await Listing.find({userRef:req.params.id})
+    res.status(200).json(listings)
+
+  }catch(error){
+    next(error)
+  }
+  }else{
+    return next(errorHandler(401,'You Can Only View Your Own Listings!'))
+  }
+};
+
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import {deleteUser, test, updateUser} from '../controllers/user.controller.js'
+import {deleteUser, test, updateUser,getUserListings} from '../controllers/user.controller.js'
 import { verifyToken } from '../utils/verifyUser.js'
 export const router = express.Router()
 router.get('/test',test)
 router.post('/update/:id',verifyToken,updateUser)
 router.delete('/delete/:id',verifyToken,deleteUser)
+router.get('/listings/:id',verifyToken,getUserListings)
 export default router
